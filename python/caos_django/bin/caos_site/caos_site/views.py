1fe8e47c18047bfc20b71eaf568d8b44a2392995
Jared M Schooley <jaschoo@siue.edu>
2018-08-18 18:28:15 -0500

    working on templates and views (extending html, etc)

--- python/caos_django/bin/caos_site/caos_site/views.py
+++ python/caos_django/bin/caos_site/caos_site/views.py
@@ -5,7 +5,7 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    template = loader.get_template('/caos/html/CAOS-Website_2018/python/caos_django/bin/caos_site/pages/templates/index.html')
+    template = loader.get_template('/caos/html/CAOS-Website_2018/python/caos_django/bin/caos_site/pages/templates/home.html')
     context = {
         'test' : 'This is also a test string',
         'test2' : ['1','2','3','4','5'],
