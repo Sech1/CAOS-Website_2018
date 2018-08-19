5d1113be731cab37eac3ad4ec1c1b25acd1d73df
Jared M Schooley <jaschoo@siue.edu>
2018-08-19 03:46:45 -0500

    settings.py

--- python/caos_virtualenv/bin/caos_site/caos_site/settings.py
+++ python/caos_virtualenv/bin/caos_site/caos_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+Temp_Path = os.path.realpath('.')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -66,6 +67,10 @@ TEMPLATES = [
         },
     },
 ]
+
+TEMPLATE_DIRS = (
+        Temp_Path +"/template"
+),
 
 WSGI_APPLICATION = 'caos_site.wsgi.application'
 
