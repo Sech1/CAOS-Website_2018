750d7ea951547750b23c9629916406e0919d3dd5
Jared M Schooley <jaschoo@siue.edu>
2018-08-19 02:09:18 -0500

    .

--- python/caos_django/bin/caos_site/caos_site/posts.py
+++ python/caos_django/bin/caos_site/caos_site/posts.py
@@ -5,7 +5,7 @@ from django.template import loader
 # Create your views here.
 
 def news_oci(request):
-    template = loader.get_template('/post/postContent/03-05-2018_eHacks_OCI.html')
+    template = loader.get_template('/post/03-05-2018_eHacks_OCI.html')
     context = {
     }
     return HttpResponse(template.render(context, request))
