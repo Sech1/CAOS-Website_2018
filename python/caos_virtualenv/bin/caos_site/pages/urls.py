f436f8c578089ed7156b5e6e43ec20823e393d7d
Jared M Schooley <jaschoo@siue.edu>
2018-08-19 05:06:09 -0500

    Stupid bug

--- python/caos_virtualenv/bin/caos_site/pages/urls.py
+++ python/caos_virtualenv/bin/caos_site/pages/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.index , name='index'),
-    url(r'^page0/$', views.page0, name='page0'),
-    url(r'^eHacks_OCI/$', views.news_oci, name='eHacks_OCI'),
+    url(r'^$', views.index , name='index'),
+    url(r'^page0/', views.page0, name='page0'),
+    url(r'^news/eHacks_OCI/', views.news_oci, name='eHacks_OCI'),
     ]
