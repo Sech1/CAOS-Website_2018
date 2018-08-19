bb782e0a345d8788340a2af9a9d792bb95d2b525
Jared M Schooley <jaschoo@siue.edu>
2018-08-19 05:26:14 -0500

    .

--- python/caos_virtualenv/bin/caos_site/pages/urls.py
+++ python/caos_virtualenv/bin/caos_site/pages/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index , name='index'),
-    path('/post/postContent/eHacks_OCI', views.news_oci, name='eHacks_OCI'),
+    path('post/postContent/eHacks_OCI', views.news_oci, name='eHacks_OCI'),
     path('page0/', views.page0, name ='page0'),
     #url(r'^post/page0/', views.page0, name='page0'),
     #url(r'^/post/postContent/eHacks_OCI/', views.news_oci, name='eHacks_OCI'),
