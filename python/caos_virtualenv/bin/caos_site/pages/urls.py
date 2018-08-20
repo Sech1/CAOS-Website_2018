ca2c5f02c44e8e106d0d0092caf819818c6e39c5
Jared M Schooley <jaschoo@siue.edu>
2018-08-20 11:37:13 -0500

    added article for MLH Localhost

--- python/caos_virtualenv/bin/caos_site/pages/urls.py
+++ python/caos_virtualenv/bin/caos_site/pages/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index , name='index'),
     path('postContent/0', views.news_oci, name='eHacks_OCI'),
+    path('postContent/1', views.mlh_localhost_drb, name='mlh_localhost'),
     path('page0/', views.page0, name ='page0'),
     path('events/', views.events, name='events'),
     path('register/', views.register, name='register'),
