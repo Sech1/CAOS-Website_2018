ab036cda7418cc5ff259da90baf554ebacccd59b
Jared M Schooley <jaschoo@siue.edu>
2018-08-22 12:13:13 -0500

    added reg validation

--- static/js/regVal.js
+++ static/js/regVal.js
@@ -13,22 +13,19 @@ function validate() {
     var lastObj = document.getElementById('id_last');
     var emailObj = document.getElementById('id_email');
 
-    document.getElementById('errorSection').innerHTML = '';
+    var errorSection = document.getElementById('errorSection')
+    errorSection.innerHTML = '';
 
     //     ---------FIRST NAME VALIDITY CHECK---------
     //Check validity of first name
     if (!inpObj.checkValidity()) {
-        //if no name is entered change placeholder
-        inpObj.setAttribute("placeholder", "Required Field - Please enter first name");
         //display error
-        document.getElementById('errorSection').innerHTML += '<span style="color:red">Enter first name.</span><br>';
+        errorSection.innerHTML += '<span style="color:red">Enter first name.</span><br>';
         //set bool to false so form will not submit
         firstValid = false;
     } else if (!inpObj.value.match(alpha)) {
-        //display error if user enters non-accepted characters
-        inpObj.setAttribute("placeholder", "Please only use alpha numeric characters");
         //display error for entering an invalid first name
-        document.getElementById('errorSection').innerHTML += '<span style="color:red">Enter valid first name.</span><br>';
+        errorSection.innerHTML += '<span style="color:red">Enter valid first name.</span><br>';
         //set bool to false so form will not submit
         firstValid = false;
     } else {
@@ -42,17 +39,13 @@ function validate() {
     //     ---------LAST NAME VALIDITY CHECK---------
     //Check validity of last name
     if (!lastObj.checkValidity()) {
-        //if no name is entered change placeholder
-        lastObj.setAttribute("placeholder", "Required Field - Please enter last name");
         //set error field to error
-        document.getElementById('errorSection').innerHTML += '<span style="color:red">Enter last name.</span><br>';
+        errorSection.innerHTML += '<span style="color:red">Enter last name.</span><br>';
         //set bool so form will not submit
         lastValid = false;
     } else if (!lastObj.value.match(alpha)) {
-        //if user enters non-alpha chars change placeholder
-        inpObj.setAttribute("placeholder", "Please only use alpha numeric characters");
         //display error in error field if non-alpha characters are used
-        document.getElementById('errorSection').innerHTML += '<span style="color:red">Enter valid last name.</span><br>';
+        errorSection.innerHTML += '<span style="color:red">Enter valid last name.</span><br>';
         //set bool to false to form will not submit
         lastValid = false;
     } else {
@@ -66,10 +59,8 @@ function validate() {
     //     ---------EMAIL VALIDITY CHECK---------
     //check validity of email input (is it an email address)
     if (!emailObj.checkValidity()) {
-        //check if email is valid - if not set placeholder to hint
-        emailObj.setAttribute("placeholder", "Required Field - Please enter email");
         //if invalid set error section to display error
-        document.getElementById('errorSection').innerHTML = '<span style="color:red">Enter valid email.</span><br>';
+        errorSection.innerHTML = '<span style="color:red">Enter valid email.</span><br>';
         //set test bool to false
         emailValid = false;
     } else {
