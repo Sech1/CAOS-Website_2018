6fd565330fdff338304feed65048791e8ed0a4c5
Jared M Schooley <jaschoo@siue.edu>
2018-08-22 01:56:57 -0500

    model for form

--- python/caos_virtualenv/bin/caos_site/pages/models.py
+++ python/caos_virtualenv/bin/caos_site/pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Registration(models.Model):
+    first = models.CharField("First", max_length=50)
+    last = models.CharField("Last", max_length=50)
+    email = models.EmailField()
